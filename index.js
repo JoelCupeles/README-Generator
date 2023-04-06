3bb1d1253bf67dd8ee0f4330e8fc77592f2dbc6b
JoelCupeles <joel.cupeles1998@gmail.com>
2023-04-06 10:11:53 -0500

    Type, Name, and Messege

--- index.js
+++ index.js
@@ -4,7 +4,60 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?', 
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a brief description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions:',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information:',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please provide contribution guidelines:',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide test instructions:',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose a license for your application:',
+        choices: [
+          'MIT',
+          'Apache 2.0',
+          'GPL 3.0',
+          'BSD 3-Clause',
+          'None',
+        ],
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username:',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address:',
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
