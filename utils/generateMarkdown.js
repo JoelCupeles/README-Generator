60055d57db1824e7cde5455c1325710c61fb9c28
JoelCupeles <joel.cupeles1998@gmail.com>
2023-04-10 09:59:06 -0500

    README generate markwon

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,32 +1,67 @@
 function renderLicenseBadge(license) {
-  if (license == 'None') {
+  if (license === 'None') {
     return '';
   }
-  return `![License badge](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
 }
 
 function renderLicenseLink(license) {
-  if (license == 'None') {
+  if (license === 'None') {
     return '';
   }
-  return `http://opensource.org/licenses/${license}`;
+  return `https://opensource.org/licenses/${license}`;
 }
 
 function renderLicenseSection(license) {
-  if (license == 'None') {
+  if (license === 'None') {
     return '';
   }
   return `
 ## License
 
-This project has the ${license} License. For more details, check the [LICENSE](${renderLicenseLink(license)}) file.
+This project is licensed under the ${license} License. For more information, see the [LICENSE](${renderLicenseLink(license)}) file.
 `;
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
 
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+For any questions, please reach out to me via [GitHub](https://github.com/${data.github}) or [email](mailto:${data.email}).
 `;
 }
 
